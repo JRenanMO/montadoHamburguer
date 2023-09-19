9ef745c1f0d06ce038199891c03788f491ff781b
JRenanMO <j.renan007@hotmail.com>
2023-09-19 17:37:09 -0300

    Hamburgue

---
+++ js/script.js
@@ -0,0 +1,104 @@
+const vm = new Vue({
+    el: "#app",
+    data: {
+        inputTipoPao: '',
+        inputSalada: [],
+        inputMolhos: [],
+        inputHamburguer: '',
+        etapa: 1,
+        inputNome: '',
+        inputEndereco:'',
+        novoPedidoAssincrono: null
+
+    },
+    computed: {
+        pao() {
+            switch (this.inputTipoPao) {
+                case 'gergelim':
+                    return [
+                        "imagens/pao_gergelim_superior.png", 
+                        "imagens/pao_gergelim_inferior.png"
+                    ]
+                case 'australiano':
+                    return [
+                        "imagens/pao_australiano_superior.png", 
+                        "imagens/pao_australiano_inferior.png"
+                    ]
+                default:
+                    return [
+                        "imagens/padrao/pao_superior.png", 
+                        "imagens/padrao/pao_inferior.png"
+                    ]
+            }
+
+        },
+        alface() {
+            if(this.inputSalada.includes('alface'))
+                return "imagens/alface.png"
+            else
+                return "imagens/padrao/alface.png"
+        },
+        ketchup() {
+            if(this.inputMolhos.includes('ketchup'))
+                return "imagens/ketchup.png"
+            else
+                return "imagens/padrao/molho.png"
+        },
+        mostarda() {
+            if(this.inputMolhos.includes('mostarda'))
+                return "imagens/mostarda.png"
+            else
+                return "imagens/padrao/molho.png"
+        },
+        maionese() {
+            if(this.inputMolhos.includes('maionese'))
+                return "imagens/maionese.png"
+            else
+                return "imagens/padrao/molho.png"
+        },
+        hamburguer() {
+            switch (this.inputHamburguer) {
+                case 'bovino':
+                    return "imagens/bovino.png"
+                case 'frango':
+                    return "imagens/frango.png"
+                case 'soja':
+                    return "imagens/soja.png"
+                default :
+                return "imagens/padrao/hamburguer.png"
+
+            }
+        },
+    },
+    methods: {
+        fazerPedido() {
+            if(this.inputTipoPao && this.inputHamburguer)
+               this.etapa = 2
+            else
+             alert('Você precisa seleciona pelo menos o pão e o hambúguer')
+        },
+        confirmarPedido() {
+            if(this.inputNome && this.inputEndereco) {
+                this.etapa = 3
+                this.novoPedidoAssincrono = setTimeout(() => this.novoPedido(), 7000)
+            } else {
+                alert('Vocé precisa informa seu nome e endereço')
+            }
+        },
+        novoPedido() {
+            this.etapa = 1
+            this.inputTipoPao = ''
+            this.inputSalada = []
+            this.inputMolhos = []
+            this.inputHamburguer = ''
+            this.inputNome = ''
+            this.inputEndereco = ''
+        },
+    },
+    watch: {
+        etapa(novoValor) {
+            if(novoValor == 1)
+            clearTimeout(this.novoPedidoAssincrono)
+        }
+    }
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Montado Hamburguer pelo vue.js
+
+## Montando Hamburguer aonde que adicionar os elementos e colocacando o nome e enderenço e pode repeti!
+
+### Projeto feito no curso da Soitech com Professor Denny Azevedo.
+
+[![Watch the video](/hambu.png)](https://youtu.be/5_hI01PV320?si=tjc6Cl2dX3H0eNhq)
